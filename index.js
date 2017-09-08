3cda24a4d4e373503591809367cb514c7c74f886
Mike Fix <mrfix84@gmail.com>
2017-09-07 18:56:19 -0700

    1.0.0

---
+++ index.js
@@ -0,0 +1,30 @@
+const morph = require('morphmorph')
+
+const extract = err => {
+  // Matches: `field "<fieldName"`
+  const extractFieldsRex = `field "(\\w+)"`
+  // Matches: `Expected "<field>", found <value>.<message>`
+  const extractValuesRex = `Expected (type.)*"(.+?)", found (.+?)(\\.|:).{0,1}(.*)`
+
+  const lines = err.message.split('\n')
+  const fields = lines
+    .map(line => line.match(new RegExp(extractFieldsRex, 'g')))
+    .filter(i => i)
+    .map(m =>
+      m.map(v => RegExp(extractFieldsRex).exec(v))
+       .map(v => v[1]))
+
+  const values = lines
+    .map(line => line.match(new RegExp(extractValuesRex, 'g')))
+    .filter(i => i)
+    .map(m => RegExp(extractValuesRex).exec(m))
+    .filter(i => i)
+    .map(v => ({
+      type: v[2],
+      message: v[5]
+    }))
+
+  return fields.reduce((accum, key, i) => morph.assign(key.join('.'))(accum, values[i]), {})
+}
+
+module.exports = exports.default = extract
---
+++ index.spec.js
@@ -0,0 +1,23 @@
+const extractFields = require('.')
+describe('Extract Errors', () => {
+  it('should extract field types and messages from error responses', () => {
+    const err = {
+      message: 'Variable "$input" got invalid value {"id":"3","address":{"zip":"1","state":"MN","streetAddress1":"Test"}}.\nIn field "address": In field "city": Expected "String!", found null.\nIn field "address": In field "zip": Expected type "ZipCode", found "1": 00001 is not a valid zipcode'
+    }
+    const { address } = extractFields(err)
+    expect(address.city).toEqual({ type: 'String!', message: '' })
+    expect(address.zip).toEqual({ type: 'ZipCode', message: '00001 is not a valid zipcode' })
+  })
+
+  it('should return an empty object if no matches are found', () => {
+    const err = { message: 'Error Message that doesn\'t match' }
+    const summary = extractFields(err)
+    expect(summary).toEqual({})
+  })
+
+  it('should not fail if #fields matched does not match #values matched', () => {
+    const err = { message: 'In field "address": In field "city"' }
+    const {address} = extractFields(err)
+    expect(address.city).toBeUndefined()
+  })
+})
